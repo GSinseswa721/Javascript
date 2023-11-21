38fb1dadc879238770fa70b0d42b28ac5684dab5
Gsinseswa721 <glorianiyonkurusinseswa@gmail.com>
2023-11-21 16:24:04 +0200

    string variable

---
+++ assign.js
@@ -0,0 +1,17 @@
+// Setup
+var myVar;
+var a;
+myVar = 5;
+ a = 7;
+
+// Only change code below this line
+
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+var b = a;
+var myVar = 0;
+var a = 9;
